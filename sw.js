e294900b6d072af7e5d700ce44d684f166b2728c
kz-20000606 <kazuya20021103@gmail.com>
2026-09-23 13:06:54 +0900

    Show vocabulary by level in word list

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='vocab-deck-v7';
-const ASSETS=['./','index.html','style.css?v=4','app.js?v=6','words.json','manifest.webmanifest','icon.svg'];
+const CACHE='vocab-deck-v8';
+const ASSETS=['./','index.html','style.css?v=5','app.js?v=7','words.json','manifest.webmanifest','icon.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
