1723931dc6af40820109a308d8860a695e09b872
kz-20000606 <kazuya20021103@gmail.com>
2026-09-23 12:21:19 +0900

    Make answer feedback clearer in sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='vocab-deck-v1';
+const CACHE='vocab-deck-v2';
 const ASSETS=['./','index.html','style.css','app.js','words.json','manifest.webmanifest','icon.svg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
